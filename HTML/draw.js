fe11adab675f667b23dc4c48828539ee0d474ec8
George D <George D>
2016-09-09 02:37:55 -0700

    Yee

---
+++ HTML/draw.js
@@ -0,0 +1,12 @@
+console.log('file works');
+
+var canvas12 = document.getElementById('myCanvas');
+var ctx = canvas12.getContext('2d');
+
+ctx.fillStyle = "rgb(250,2,3)";
+ctx.fillRect(100,100,100,50);
+ctx.strokeStyle = "rgb(175,125,0)";
+ctx.strokeRect(80,80,140,140);
+ctx.clearRect(120,120,60,60);
+
+//C4 Studios is bad. Mojang is better
