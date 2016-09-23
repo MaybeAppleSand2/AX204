e07724fb345bf2cb05ada2d8bb91ab4ce7a37fdf
George D <George D>
2016-09-23 17:38:38 +0800

    newblog

--- HTML/draw.js
+++ HTML/draw.js
@@ -9,4 +9,9 @@ ctx.strokeStyle = "rgb(175,125,0)";
 ctx.strokeRect(80,80,140,140);
 ctx.clearRect(120,120,60,60);
 
-//C4 Studios is bad. Mojang is better
+//C4 Studios is bad. Mojang is better. Illumati is real. 
+
+
+//git add
+//git commit -m "masg"
+//git push origin master
